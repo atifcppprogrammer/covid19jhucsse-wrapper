fe2b02f35d540801ee07ce8dfa10760387040250
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2020-11-13 15:18:41 +0500

    parse.js module added

---
+++ modules/parse.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+const path = require('path');
+const fs = require('fs');
+
+const pathToCache = path.join(__dirname, '..', 'cache');
+
+const extractDates = lineContentArray => lineContentArray.splice(4)
+
+const extractDatasetRow = (dataset, lineContentArray) => {
+  const [ region, country ] = lineContentArray.slice(0, 2);
+  const figures = lineContentArray.splice(4);
+  if (region !== '') {
+    if (dataset[country]) dataset[country] = { 
+      regions: { ...dataset[country].regions, [region]: figures } };
+    else dataset[country] = { regions:{ [region]: figures } };
+  }
+  else dataset[country] = figures;
+}
+
+const parseDataset = type => new Promise(resolve => {
+  const reader = readline.createInterface({
+    input: fs.createReadStream(path.join(pathToCache, `${type}.csv`))
+  });
+  let dataset = {}, count = 0;
+  reader.on('line', lineContent => {
+    lineContentArray = lineContent.split(',');
+    if (count === 0) 
+      dataset.dates = extractDates(lineContentArray);
+    else extractDatasetRow(dataset, lineContentArray);
+    ++count;
+  });
+  reader.on('close', () => resolve(dataset));
+});
+
+const saveParsedDataset = async (type, json) => {
+  const jsonFilePath = path.join(pathToCache, `${type}.json`);
+  const csvFilePath = path.join(pathToCache, `${type}.csv`);
+
+  await fs.promises.unlink(csvFilePath);
+  return fs.promises.writeFile(jsonFilePath, JSON.stringify(json));
+}
+
+module.exports = () => {
+  const promises = [ 'confirmed', 'deaths', 'recovered' ]
+    .map(type => parseDataset(type)
+      .then(json => saveParsedDataset(type, json)));
+  return Promise.all(promises);
+}
