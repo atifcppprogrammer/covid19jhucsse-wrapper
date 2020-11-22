7612aa9825f1fc7bc2d09b1d6c71f5cb85359de4
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2020-11-23 01:18:52 +0500

    datasetInitializationFailed added to errors.js

--- modules/errors.js
+++ modules/errors.js
@@ -3,3 +3,9 @@ exports.networkRequestFailed = () => {
           .concat('from JHU-CSSE\'s GitHub Repository failed !');
   return new Error(message);
 }
+
+exports.datasetInitializationFailed = () => {
+  const message = 'JHU-CSSE Dataset initialization failed owing '
+          .concat('to network error !');
+  return new Error(message);
+}
